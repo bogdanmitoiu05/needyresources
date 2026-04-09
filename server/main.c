2664a052bced0d81b80ec837cf3ca15873bf309f
bogdanmitoiu05 <202067756+bogdanmitoiu05@users.noreply.github.com>
2026-04-09 19:59:27 +0300

    Some changes

--- server/main.c
+++ server/main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/shm.h>
 #include <pthread.h>
-int main()
+
+
+/**
+ * Sistemul se va baza pe o coadă de mesaje ce va co
+ */
+int main(int argc, const char** argv)
 {
 
 }
