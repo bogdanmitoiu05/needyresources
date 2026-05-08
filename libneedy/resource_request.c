3c63f2270968110607b6cb0ffab980d851003d49
bogdanmitoiu05 <202067756+bogdanmitoiu05@users.noreply.github.com>
2026-05-08 17:06:21 +0000

    Keep consistency

--- libneedy/resource_request.c
+++ libneedy/resource_request.c
@@ -22,7 +22,7 @@ __attribute_warn_unused_result__ needy_resource_request * nr_client_resource_req
     pid_t pid = 0;
     size_t requestedResources = 0;
     json_error_t error;
-    int success = json_unpack_ex(data, &error, JSON_STRICT, "{s:I,s:I}","pid",&pid, "requested_resources",&requestedResources);
+    int success = json_unpack_ex(data, &error, JSON_STRICT, "{s:I,s:I}","pid",&pid, "requestedResources",&requestedResources);
     if (!success) {
         fputs("nr_client_resource_deserialize: invalid JSON",stderr);
         JSON_ERROR_PRINTF(error);
@@ -35,7 +35,7 @@ __attribute_warn_unused_result__ json_t* nr_client_resource_request_serialize(co
     ENSURE_NOTNULL_MSG_RNULL(this, "nr_client_resource_serialize: NULL pointer passed");
 
     json_error_t error;
-    json_t* result = json_pack_ex(&error, 0, "{s:I,s:I}","pid",this->pid, "requested_resources", this->requestedResources);
+    json_t* result = json_pack_ex(&error, 0, "{s:I,s:I}","pid",this->pid, "requestedResources", this->requestedResources);
     if (!result) {
         fputs("nr_client_resource_request_serialize: JSON Error",stderr);
         JSON_ERROR_PRINTF(error);
