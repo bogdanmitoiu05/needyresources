03dbe6812f2c1e51061082d09489cb1cb2ab838f
bogdanmitoiu05 <202067756+bogdanmitoiu05@users.noreply.github.com>
2026-05-12 20:29:44 +0000

    fix message bug

--- libneedy/message.c
+++ libneedy/message.c
@@ -68,7 +68,7 @@ needy_message_t* needy_message_from_string(char* str) {
         JSON_ERROR_PRINTF(error);
         return NULL;
     }
-    return needy_message_deserialize(msgJson); //returnea
+    return needy_message_deserialize(msgJson); //returnează me
 }
 void needy_message_destroy(needy_message_t *msg) {
     ENSURE_NOTNULL(msg);
