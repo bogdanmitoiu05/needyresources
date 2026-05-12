2b1d47468fc5c1562b159c0d9dd1122ab8c503fa
bogdanmitoiu05 <202067756+bogdanmitoiu05@users.noreply.github.com>
2026-05-12 20:28:58 +0000

    Documented client

--- libneedy/message.c
+++ libneedy/message.c
@@ -9,65 +9,70 @@
 
 needy_message_t* needy_message_new(needy_message_type type, json_t *payload) {
     ENSURE_NOTNULL_MSG_RNULL(payload, "needy_message_new: payload passed was a NULL pointer");
-    if (type > N_TYPES) {
+    if (type > N_TYPES) { // dacă a fost introdus un tip de date invalid -> raportează
         printf("needy_message_new: Invalid type %u\n", type);
         return NULL;
     }
+    //alocare
     needy_message_t* message = calloc(1,  sizeof(needy_message_t));
     ENSURE_NOTNULL_MSG_RNULL(message, "needy_message_new: could not allocate message");
+    //inițializare
     message->version = NEEDY_PROTOCOL_VERSION;
     message->message_type = type;
-    message->payload = json_incref(payload);
+    message->payload = json_incref(payload); //vom marca faptul că aces obiect are o referință către încărcătură
     return message;
 
 }
 
 json_t * needy_message_serialize(needy_message_t *msg) {
-    ENSURE_NOTNULL_MSG_RNULL(msg, "needy_message_serialize: msg is NULL");
+    ENSURE_NOTNULL_MSG_RNULL(msg, "needy_message_serialize: msg is NULL"); //null guard
+    // verificare versiune protocol
     if (msg->version != NEEDY_PROTOCOL_VERSION) {
         fprintf(stderr,"Invalid protocol version %lu. Protocol version %lu needed", msg->version, (size_t) NEEDY_PROTOCOL_VERSION);
         return NULL;
     }
-    json_t* result = json_pack("{s:I,s:s,s:O}","version",msg->version,"type",msg->message_type,"payload",msg->payload);
-    ENSURE_NOTNULL_MSG_RNULL(result, "needy_message_serialize: Failed to serialize message");
+    // împachetare mesaj conform structurii din message.h
+    json_t* result = json_pack("{s:I,s:s,s:O}"/*„O” indică faptul că obiectului i se va incrementa usage counterul*/,"version",msg->version,"type",needy_message_type_to_string(msg->message_type)/*obține stringul asociat valorii message type*/,"payload",msg->payload);
+    ENSURE_NOTNULL_MSG_RNULL(result, "needy_message_serialize: Failed to serialize message"); //verificare eroare
     return result;
 }
 
 needy_message_t * needy_message_deserialize(json_t *msg) {
-    ENSURE_NOTNULL_MSG_RNULL(msg, "needy_message_deserialize: msg is NULL");
-    needy_message_t* result = calloc(1, sizeof(needy_message_t));
+    ENSURE_NOTNULL_MSG_RNULL(msg, "needy_message_deserialize: msg is NULL"); // null guard
+    needy_message_t* result = calloc(1, sizeof(needy_message_t)); // instantiere
     ENSURE_NOTNULL_MSG_RNULL(msg, "needy_message_deserialize: could not allocate message");
-    json_error_t error;
-    const char* type = NULL;
+    json_error_t error; // pregatire despachetare
+    const char* type = NULL; //vezi mai sus
     int success = json_unpack_ex(msg, &error, JSON_STRICT, "{s:I,s:s,s:O}", "version",&(result->version), "type",type, "payload",&(result->payload));
     if (success<0) {
         JSON_ERROR_PRINTF(error);
         free(result);
         return NULL;
     }
-    result->message_type = needy_message_type_from_string(type);
+    result->message_type = needy_message_type_from_string(type); //extrage tipul de mesaj din sirul de caractere aferent
     return result;
 
 }
 
 char* needy_message_to_string(needy_message_t* msg) {
-    ENSURE_NOTNULL_MSG_RNULL(msg,"needy_message_to_string: msg is NULL");
-    json_t* root = needy_message_serialize(msg);
-    return json_dumps(root, JSON_COMPACT);
+    ENSURE_NOTNULL_MSG_RNULL(msg,"needy_message_to_string: msg is NULL"); // null guard
+    json_t* root = needy_message_serialize(msg); // serializează mesajul
+    return json_dumps(root, JSON_COMPACT); // scrie îm format JSON compact (fără \n)
 }
 
 needy_message_t* needy_message_from_string(char* str) {
-    json_error_t error;
-    json_t* msgJson = json_loads(str, JSON_REJECT_DUPLICATES, &error);
-    if (!msgJson) {
+    ENSURE_NOTNULL_MSG_RNULL(str, "needy_message_from_string: str is NULL"); // null guard
+    json_error_t error; // eroare
+    json_t* msgJson = json_loads(str, JSON_REJECT_DUPLICATES, &error); // încarcă din string
+    if (!msgJson) { // la eroare, NULL
         JSON_ERROR_PRINTF(error);
         return NULL;
     }
-    return needy_message_deserialize(msgJson);
+    return needy_message_deserialize(msgJson); //returnea
 }
 void needy_message_destroy(needy_message_t *msg) {
     ENSURE_NOTNULL(msg);
-    if (msg->payload)
+    //if (msg->payload)
         //json_decref(msg->payload);
     free(msg);
 }
--- libneedy/message.h
+++ libneedy/message.h
@@ -26,15 +26,41 @@ typedef struct message {
     json_t *payload;
 }needy_message_t;
 
+/**
+ * Crează o instață de mesaj
+ * @param type tipul de mesaj (vezi message_type.h)
+ * @param payload mesajul efectiv din interior
+ * @return Un nou mesaj sau NULL la eșec
+ */
 needy_message_t* needy_message_new(needy_message_type type, json_t* payload);
+/**
+ * Serializează un mesaj în format JSON AST
+ * @param msg mesajul de serializat
+ * @return AST-ul JSON corespunzător sau NULL la eroare
+ */
 json_t* needy_message_serialize(needy_message_t* msg);
+/**
+ * Decodifică un mesaj din AST-ul JSON
+ * @param msg mesajul de decodificat
+ * @return instanța de mesaj corespunzătoare sau NULL la eroare
+ */
 needy_message_t* needy_message_deserialize(json_t* msg);
+/**
+ * Transformă mesajul în string
+ * @param msg mesaj
+ * @return Șir de caractere sau NULL la eroare
+ */
 char* needy_message_to_string(needy_message_t* msg);
+/**
+ * Decodează mesajul dintr-un string
+ * @param str string
+ * @return mesajul stocat în interior sau NULL la eroare
+ */
 needy_message_t* needy_message_from_string(char* str);
 
 /**
- * Destroys a message.
- * @param msg message to be destroyed
+ * Distruge un mesaj
+ * @param msg Mesajul ce trebuie distrus
  */
 void needy_message_destroy(needy_message_t* msg);
 #endif //NEEDYRESOURCES_MESSAGE_FRAME_H
