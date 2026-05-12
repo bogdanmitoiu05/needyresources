1ee9fd58c8d0f240551c7224a4148e5788006eb0
bogdanmitoiu05 <202067756+bogdanmitoiu05@users.noreply.github.com>
2026-05-12 20:56:50 +0000

    documented resource request

--- libneedy/resource_request.c
+++ libneedy/resource_request.c
@@ -6,7 +6,7 @@
 
 
 __attribute_warn_unused_result__ needy_resource_request* needy_client_resource_request_new(const pid_t pid, const size_t requestedResources) {
-    needy_resource_request* res = calloc(1,sizeof(needy_resource_request));
+    needy_resource_request* res = calloc(1,sizeof(needy_resource_request)); // instanțiere și verificare
     ENSURE_NOTNULL_MSG_RNULL(res, "Could not allocate resource_request");
     res->pid = pid;
     res->requestedResources = requestedResources;
@@ -14,28 +14,29 @@ __attribute_warn_unused_result__ needy_resource_request* needy_client_resource_r
 }
 
 __attribute_warn_unused_result__ needy_resource_request * needy_client_resource_request_deserialize(json_t *data) {
-    ENSURE_NOTNULL_MSG_RNULL(data, "needy_client_resource_deserialize: NULL pointer passed");
+    ENSURE_NOTNULL_MSG_RNULL(data, "needy_client_resource_deserialize: NULL pointer passed"); // null guard
     if (!json_is_object(data)) {
         fputs("needy_client_resource_deserialize: Data passed not an object",stderr);
         return NULL;
     }
     pid_t pid = 0;
     size_t requestedResources = 0;
-    json_error_t error;
-    int success = json_unpack_ex(data, &error, JSON_STRICT, "{s:I,s:I}","pid",&pid, "requestedResources",&requestedResources);
+    json_error_t error; // deserializare
+    int success = json_unpack_ex(data, &error, JSON_STRICT, "{s:I,s:I}"/*I = long int, s = string*/,"pid",&pid, "requestedResources",&requestedResources);
     if (success<0) {
         fputs("needy_client_resource_deserialize: invalid JSON",stderr);
         JSON_ERROR_PRINTF(error);
     }
+    // instanțiere cu noile date
     needy_resource_request* resource_request = needy_client_resource_request_new(pid,requestedResources);
     return resource_request;
 }
 
 __attribute_warn_unused_result__ json_t* needy_client_resource_request_serialize(const needy_resource_request *this) {
-    ENSURE_NOTNULL_MSG_RNULL(this, "needy_client_resource_serialize: NULL pointer passed");
+    ENSURE_NOTNULL_MSG_RNULL(this, "needy_client_resource_serialize: NULL pointer passed"); //procesul de mai sus, invers
 
     json_error_t error;
-    json_t* result = json_pack_ex(&error, 0, "{s:I,s:I}","pid",this->pid, "requestedResources", this->requestedResources);
+    json_t* result = json_pack_ex(&error, 0, "{s:I,s:I}"/*idem*/,"pid",this->pid, "requestedResources", this->requestedResources);
     if (!result) {
         fputs("needy_client_resource_request_serialize: JSON Error",stderr);
         JSON_ERROR_PRINTF(error);
@@ -44,6 +45,6 @@ __attribute_warn_unused_result__ json_t* needy_client_resource_request_serialize
 }
 
 void needy_client_resource_request_destroy(needy_resource_request *this) {
-    ENSURE_NOTNULL(this);
+    ENSURE_NOTNULL(this); // null
     free(this);
 }
