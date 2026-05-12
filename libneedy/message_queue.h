a26a0fdfc16cf844bf219e0d0e168551281c0992
Al-Tex <alexandruturculet2006@gmail.com>
2026-05-12 16:55:36 +0000

    address server queue permission denied

--- libneedy/message_queue.h
+++ libneedy/message_queue.h
@@ -5,7 +5,7 @@
 #ifndef NEEDYRESOURCES_MESSAGE_QUEUE_H
 #define NEEDYRESOURCES_MESSAGE_QUEUE_H
 
-#define SERVER_QUEUE_NAME "/tmp/m/needy_server_mq"
+#define SERVER_QUEUE_NAME "/needy_server_mq"
 #define PROJECT_ID 'M'
 #define QUEUE_PERMISSIONS 0660
 #define MAX_MSG_SIZE 8192
