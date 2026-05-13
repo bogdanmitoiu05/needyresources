faad44dcf650d2bea6f34709eca3ffa1fc02737e
bogdanmitoiu05 <202067756+bogdanmitoiu05@users.noreply.github.com>
2026-05-13 11:03:47 +0300

    Docs final

--- server/mq_manager.c
+++ server/mq_manager.c
@@ -7,19 +7,19 @@
 
 client_conn* client_conn_new(needy_client_identification_header* clientInfo)
 {
-    client_conn* cConn = new(client_conn);
-    struct mq_attr attr;
-    attr.mq_flags = 0;
-    attr.mq_maxmsg = 5;
-    attr.mq_msgsize = MAX_MSG_SIZE;
+    client_conn* cConn = new(client_conn); //instanțiere
+    struct mq_attr attr; // atributele cozii de mesaje
+    attr.mq_flags = 0; // fără flags
+    attr.mq_maxmsg = 5; // max. 5 mesaje
+    attr.mq_msgsize = MAX_MSG_SIZE; // fiecare mesaj max MAX_MSG_SIZE octeți
     attr.mq_curmsgs = 0;
     ENSURE_NOTNULL_MSG_RNULL(cConn, "client_conn_new: could not allocate new client connection object");
 
-    char client_queue_name[64];
+    char client_queue_name[64]; //coada clientului cu PID x este /needy_client_mq_$x
     snprintf(client_queue_name, sizeof(client_queue_name), "/needy_client_mq_%d", clientInfo->pid);
 
-    mqd_t client_queue = mq_open(client_queue_name, O_CREAT | O_WRONLY, 0644, &attr);
-    if (client_queue < 0)
+    mqd_t client_queue = mq_open(client_queue_name, O_CREAT | O_WRONLY, 0644, &attr); //deschide coada procesului pentru scriere
+    if (client_queue < 0) // eroare
     {
         perror("client_conn_new could not open client pipe: ");
         return NULL;
@@ -29,7 +29,7 @@ client_conn* client_conn_new(needy_client_identification_header* clientInfo)
     return cConn;
 }
 mqd_t findByPid(MQManager* mgr,pid_t pid) {
-    for (size_t i = 0; i < mgr->queue_size; i++) {
+    for (size_t i = 0; i < mgr->queue_size; i++) { // caută prima (și singura) coadă coresp. PID-ului procesului
         if (mgr->queues[i]->clientInfo->pid==pid) {
             return mgr->queues[i]->queue;
         }
@@ -37,12 +37,12 @@ mqd_t findByPid(MQManager* mgr,pid_t pid) {
 }
 void client_conn_destroy(client_conn* conn)
 {
-    ENSURE_NOTNULL(conn);
+    ENSURE_NOTNULL(conn); // nu apela free pe null
     mq_close(conn->queue);
     needy_client_identification_header_destroy(conn->clientInfo);
 }
 MQManager* mq_manager_new(size_t size) {
-    MQManager* mgr = new(MQManager);
+    MQManager* mgr = new(MQManager); //instanțiere
     ENSURE_NOTNULL_MSG_RNULL(mgr,"mq_manager_new: could not allocate MQManager");
     mgr->queues = calloc(size, sizeof(client_conn*));
     mgr->queue_size = size;
@@ -56,12 +56,12 @@ MQManager* mq_manager_new(size_t size) {
 }
 
 int mq_manager_add(MQManager *mgr, client_conn* mq) {
-    ENSURE_NOTNULL_MSG_RETVAL(mgr, "mq_manager_add: mgr null", -1);
+    ENSURE_NOTNULL_MSG_RETVAL(mgr, "mq_manager_add: mgr null", -1); //null guard
     if (mgr->active_count >= mgr->queue_size) {
         return -1;
     }
 
-    for (size_t i = 0; i < mgr->queue_size; i++) {
+    for (size_t i = 0; i < mgr->queue_size; i++) { // introduce conexiunea în primul loc liber din vector
         if (mgr->queues[i] == NULL) {
             mgr->queues[i] = mq;
             mgr->active_count++;
@@ -72,9 +72,9 @@ int mq_manager_add(MQManager *mgr, client_conn* mq) {
 }
 
 int mq_manager_remove(MQManager *mgr, client_conn* mq) {
-    ENSURE_NOTNULL_MSG_RETVAL(mgr, "mq_manager_remove: mgr null", -1);
+    ENSURE_NOTNULL_MSG_RETVAL(mgr, "mq_manager_remove: mgr null", -1); //null guard
     for (size_t i = 0; i < mgr->queue_size; i++) {
-        if (mgr->queues[i] == mq) {
+        if (mgr->queues[i] == mq) { //găsește procesul și ștergere-l
             client_conn_destroy(mgr->queues[i]);
             mgr->queues[i] = NULL;
             mgr->active_count--;
--- server/mq_manager.h
+++ server/mq_manager.h
@@ -6,23 +6,72 @@
 #define NEEDYRESOURCES_MQ_MANAGER_H
 #include <needy.h>
 
+/**
+ * Structura ce reprezinta o conexiune
+ * O conexiune se defineste in server ca fiind o pereche (antet, queue_fd)
+ */
 typedef struct
 {
     mqd_t queue;
     needy_client_identification_header* clientInfo;
 } client_conn;
+
+/**
+ * Structura gestiune cozi
+ *
+ */
 typedef struct {
-    client_conn** queues;
-    size_t queue_size;
-    size_t active_count;
+    client_conn** queues; //cozi
+    size_t queue_size; // nr. cozi
+    size_t active_count; //cate procese sunt in asteptare
 } MQManager;
 
+/**
+ * Gaseste conexiunea dupa PID
+ * @param mgr obiectul manager
+ * @param pid identificator proces
+ * @return ID-ul găsit sau -1 dacă nu există
+ */
 mqd_t findByPid(MQManager* mgr,pid_t pid);
+/**
+ * Instanțiază o conexiune nouă de client
+ * @param clientInfo antet client
+ * @return conexiunea nouă sau NULL
+ */
 client_conn* client_conn_new(needy_client_identification_header* clientInfo);
+/**
+ * Destructor
+ * @param conn conexiunea de distrus
+ */
 void client_conn_destroy(client_conn* conn);
+/**
+ * Instanțiere manager cozi
+ * @param size nr. maxim de procese admise
+ * @return manager sau NULL la eroare
+ */
 MQManager* mq_manager_new(size_t size);
+/**
+ * Adaugă un proces în gestiune
+ * @param mgr manager
+ * @param conn conexiune
+ * @return 0 la OK, -1 la eroare
+ */
 int mq_manager_add(MQManager *mgr, client_conn* conn);
+/**
+ * Șterge un proces din gestiune
+ * @param mgr manager
+ * @param conn conexiunea aferentă
+ * @return 0 la OK, -1 la eroare
+ */
 int mq_manager_remove(MQManager *mgr, client_conn* conn);
+/**
+ * Închide toate conexiunile
+ * @param mgr manager
+ */
 void mq_manager_close_all(MQManager *mgr);
+/**
+ * Destructor manager
+ * @param mgr obiectul de distrus
+ */
 void mq_manager_destroy(MQManager* mgr);
 #endif //NEEDYRESOURCES_MQ_MANAGER_H
